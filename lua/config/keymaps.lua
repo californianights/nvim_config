dd4a3bfe091e7cc1a2d4b8775c4600c828175121
I-AM-LAMER <vadimtatarnikov300605@gmail.com>
2026-05-12 17:41:02 +0300

    feat: add terminal plugin, change transparency parameter

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,11 +9,14 @@ map("n", "<leader>h", "<cmd>nohlsearch<cr>", { desc = "Clear search highlight" }
 map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Open Lazy" })
 map("n", "<leader>m", "<cmd>Mason<cr>", { desc = "Open Mason" })
 map("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Close buffer" })
+map("n", "<leader>tt", "<cmd>ToggleTerm<cr>", { desc = "Toggle terminal" })
 
 map("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
 map("n", "<C-j>", "<C-w>j", { desc = "Move to lower window" })
 map("n", "<C-k>", "<C-w>k", { desc = "Move to upper window" })
 map("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+
+map("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 map("n", "<leader>-", "<cmd>split<cr>", { desc = "Horizontal split" })
 map("n", "<leader>|", "<cmd>vsplit<cr>", { desc = "Vertical split" })
--- lua/config/plugins/editor.lua
+++ lua/config/plugins/editor.lua
@@ -14,4 +14,15 @@ return {
         event = "VeryLazy",
         opts = {},
     },
+    {
+        "akinsho/toggleterm.nvim",
+        version = "*",
+        cmd = { "ToggleTerm", "TermExec" },
+        opts = {
+            direction = "horizontal",
+            size = 14,
+            start_in_insert = true,
+            insert_mappings = false,
+        },
+    },
 }
