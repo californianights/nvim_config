ea3bab9762f991a7d82a109f07e96b830a880e4b
I-AM-LAMER <vadimtatarnikov300605@gmail.com>
2026-05-12 18:13:08 +0300

    feat: add markdown preview plugin

--- lua/config/plugins/editor.lua
+++ lua/config/plugins/editor.lua
@@ -25,4 +25,12 @@ return {
             insert_mappings = false,
         },
     },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+    },
 }
